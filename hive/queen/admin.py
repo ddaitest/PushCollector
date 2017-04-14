0bd80101df05afeecbcc46e77b0bdb35e6d78d02
sunny.dai <ddai_android@126.com>
2017-04-14 15:12:57 +0800

    oo

--- hive/queen/admin.py
+++ hive/queen/admin.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Question,Message,MsgRecord
+from .models import Abc,Message,MsgRecord
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Abc)
 admin.site.register(Message)
-admin.site.register(MsgRecord)
+#admin.site.register(MsgRecord)
--- hive/queen/models.py
+++ hive/queen/models.py
@@ -6,7 +6,7 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 @python_2_unicode_compatible  # only if you need to support Python 2
-class Question(models.Model):
+class Abc(models.Model):
     question_text = models.CharField(max_length=200)
     def __str__(self):
         return self.question_text
@@ -15,7 +15,7 @@ class Question(models.Model):
 class Message(models.Model):
     title = models.CharField(max_length=200)
     cotent = models.CharField(max_length=200)
-    created_on = models.DateTimeField('created on')
+    created_on = models.DateTimeField('created')
     def __str__(self):
         return self.title
 
@@ -25,7 +25,7 @@ class MsgRecord(models.Model):
     plaform = models.CharField(max_length=200)
     token = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
-    sent_on = models.DateTimeField('sent on')
-    reach_on = models.DateTimeField('reach on')
+    sent_on = models.DateTimeField('sent')
+    reach_on = models.DateTimeField('reach')
     def __str__(self):
         return self.token

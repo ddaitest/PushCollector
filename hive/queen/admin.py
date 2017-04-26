b7a9d3fdb93b8330aa4fe06f4b3d216884cff9d8
QDHL <qdhl@daining.local>
2017-04-26 18:55:47 +0800

    ok

--- hive/queen/admin.py
+++ hive/queen/admin.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Abc,Message,MsgRecord
+from .models import Abc,Message,MsgRecord,Token
 # Register your models here.
-admin.site.register(Abc)
+admin.site.register(Token)
 admin.site.register(Message)
 admin.site.register(MsgRecord)
